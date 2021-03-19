986745daa3b24d3558deda7f12770988796f2672
Edward Amor <edward.amor3@gmail.com>
2021-03-19 12:03:25 -0400

    test: add root contest w/ global fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from brownie.network.account import Account, Accounts, EthAddress
+from brownie.network.contract import Contract, ContractContainer
+
+
+@pytest.fixture(scope="module")
+def alice(accounts: Accounts) -> Account:
+    """Alice the first available test account."""
+    return accounts[0]
+
+
+@pytest.fixture(scope="module")
+def bob(accounts: Accounts) -> Account:
+    """Bob the second available test account."""
+    return accounts[1]
+
+
+@pytest.fixture(scope="module")
+def charlie(accounts: Accounts) -> Account:
+    """Charlie the third available test account."""
+    return accounts[2]
+
+
+@pytest.fixture(scope="module")
+def zero_address() -> EthAddress:
+    """The canonical ethereum zero address."""
+    return EthAddress("0x0000000000000000000000000000000000000000")
+
+
+@pytest.fixture(scope="module")
+def xhibit(alice: Account, Xhibit: ContractContainer) -> Contract:
+    """Instance of the Xhibit contract."""
+    return alice.deploy(Xhibit)
+
+
+@pytest.fixture(autouse=True)
+def isolate(fn_isolation):
+    """Isolate each test function."""
+    pass

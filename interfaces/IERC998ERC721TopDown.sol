50061e84304c1e9078bda6ad16dbe8d07022ee33
Edward Amor <edward.amor3@gmail.com>
2021-03-19 18:20:04 -0400

    fix: update interface solidity version to ^0.7.4

--- interfaces/IERC998ERC721TopDown.sol
+++ interfaces/IERC998ERC721TopDown.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.4.24;
+pragma solidity ^0.7.4;
 
 /// @title ERC998ERC721 Top-Down Composable Non-Fungible Token
 /// @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-998.md

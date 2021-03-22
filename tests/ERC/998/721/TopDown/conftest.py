ba6d4e42170563f89b408a699032b5be15698832
Edward Amor <edward.amor3@gmail.com>
2021-03-22 00:45:49 -0400

    feat: add special nft fixture

--- tests/ERC/998/721/TopDown/conftest.py
+++ tests/ERC/998/721/TopDown/conftest.py
@@ -1,12 +1,18 @@
 import pytest
 
 
-@pytest.fixture(scope="module", autouse=True)
-def setup(alice, nft, xhibit):
-    nft._mint_for_testing(alice, {"from": alice})
-    xhibit.mint(alice, {"from": alice})
-
-
 @pytest.fixture(scope="module")
 def composable_top_down(alice, ComposableTopDown):
     return alice.deploy(ComposableTopDown)
+
+
+@pytest.fixture(scope="module")
+def nft_transfer_last(alice, ERC721TransferLast):
+    return alice.deploy(ERC721TransferLast)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup(alice, nft, nft_transfer_last, xhibit):
+    nft_transfer_last._mint_for_testing(alice, {"from": alice})
+    nft._mint_for_testing(alice, {"from": alice})
+    xhibit.mint(alice, {"from": alice})

94f81fe38ea14a65c233f20985e74447c73691ce
Edward Amor <edward.amor3@gmail.com>
2021-03-21 21:20:06 -0400

    feat: add composable contract fixture

--- tests/ERC/998/721/TopDown/conftest.py
+++ tests/ERC/998/721/TopDown/conftest.py
@@ -5,3 +5,8 @@ import pytest
 def setup(alice, nft, xhibit):
     nft._mint_for_testing(alice, {"from": alice})
     xhibit.mint(alice, {"from": alice})
+
+
+@pytest.fixture(scope="module")
+def composable_top_down(alice, ComposableTopDown):
+    return alice.deploy(ComposableTopDown)

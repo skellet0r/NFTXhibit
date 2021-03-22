3e2490051651280584b8f0aa8307449e5bbfc15d
Edward Amor <edward.amor3@gmail.com>
2021-03-22 01:27:09 -0400

    test: getChild revert cases

--- tests/ERC/998/721/TopDown/test_getChild.py
+++ tests/ERC/998/721/TopDown/test_getChild.py
@@ -22,3 +22,23 @@ def test_getChild_unsuccessful_invalid_caller(alice, bob, nft, xhibit):
 
     with brownie.reverts("dev: Caller is neither _childTokenId owner nor operator"):
         xhibit.getChild(alice, 0, nft, 0, {"from": bob})
+
+
+def test_unsuccessful_transfer_parent_token_non_existent(alice, nft, xhibit):
+    nft.approve(xhibit, 0, {"from": alice})
+
+    with brownie.reverts("dev: Recipient token non-existent"):
+        xhibit.getChild(alice, 1, nft, 0, {"from": alice})
+
+
+def test_unsuccessful_transfer_receiving_token_already_possessed(
+    alice, nft_no_auth, xhibit
+):
+    nft_no_auth.approve(xhibit, 0, {"from": alice})
+    xhibit.getChild(alice, 0, nft_no_auth, 0, {"from": alice})
+
+    nft_no_auth.transferFrom(xhibit, alice, 0, {"from": alice})
+    nft_no_auth.approve(xhibit, 0, {"from": alice})
+
+    with brownie.reverts("dev: Child token already possessed"):
+        xhibit.getChild(alice, 0, nft_no_auth, 0, {"from": alice})

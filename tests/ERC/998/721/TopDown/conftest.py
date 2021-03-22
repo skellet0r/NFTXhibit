ba4b07e295d965344664d004ceff7277b7878b0f
Edward Amor <edward.amor3@gmail.com>
2021-03-22 01:05:02 -0400

    test: add no auth ERC721 fixture

--- tests/ERC/998/721/TopDown/conftest.py
+++ tests/ERC/998/721/TopDown/conftest.py
@@ -11,8 +11,14 @@ def nft_transfer_last(alice, ERC721TransferLast):
     return alice.deploy(ERC721TransferLast)
 
 
+@pytest.fixture(scope="module")
+def nft_no_auth(alice, ERC721NoAuth):
+    return alice.deploy(ERC721NoAuth)
+
+
 @pytest.fixture(scope="module", autouse=True)
-def setup(alice, nft, nft_transfer_last, xhibit):
+def setup(alice, nft, nft_no_auth, nft_transfer_last, xhibit):
+    nft_no_auth._mint_for_testing(alice, {"from": alice})
     nft_transfer_last._mint_for_testing(alice, {"from": alice})
     nft._mint_for_testing(alice, {"from": alice})
     xhibit.mint(alice, {"from": alice})

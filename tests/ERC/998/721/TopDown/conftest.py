b32f29b7065e9b68ab486109bb0e2c93a44f29a6
Edward Amor <edward.amor3@gmail.com>
2021-03-23 16:37:51 -0400

    feat(test): add composable_bottom_down fixture

--- tests/ERC/998/721/TopDown/conftest.py
+++ tests/ERC/998/721/TopDown/conftest.py
@@ -7,6 +7,11 @@ def composable_top_down(alice, ComposableTopDown):
 
 
 @pytest.fixture(scope="module")
+def composable_bottom_down(alice, ComposableBottomDown):
+    return alice.deploy(ComposableBottomDown)
+
+
+@pytest.fixture(scope="module")
 def nft_transfer_last(alice, ERC721TransferLast):
     return alice.deploy(ERC721TransferLast)
 
@@ -17,7 +22,8 @@ def nft_no_auth(alice, ERC721NoAuth):
 
 
 @pytest.fixture(scope="module", autouse=True)
-def setup(alice, nft, nft_no_auth, nft_transfer_last, xhibit):
+def setup(alice, composable_bottom_down, nft, nft_no_auth, nft_transfer_last, xhibit):
+    composable_bottom_down._mint_for_testing(alice, {"from": alice})
     nft_no_auth._mint_for_testing(alice, {"from": alice})
     nft_transfer_last._mint_for_testing(alice, {"from": alice})
     nft._mint_for_testing(alice, {"from": alice})

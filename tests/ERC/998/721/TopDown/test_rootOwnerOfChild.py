26eb647a790f0e2f9d35620124e6f3b035d7638d
Edward Amor <edward.amor3@gmail.com>
2021-03-23 01:54:29 -0400

    test: composable root owner

--- tests/ERC/998/721/TopDown/test_rootOwnerOfChild.py
+++ tests/ERC/998/721/TopDown/test_rootOwnerOfChild.py
@@ -23,3 +23,22 @@ def test_root_owner_is_contract(alice, nft_no_auth, xhibit):
     root_owner = xhibit.rootOwnerOfChild(nft_no_auth, 0)
 
     assert root_owner == expected_root_owner
+
+
+def test_root_owner_is_top_down_composable(
+    alice, composable_top_down, nft_no_auth, xhibit
+):
+    # mint an xhibit for the mock Top Down contract
+    xhibit.mint(composable_top_down, {"from": alice})
+    # alice sends her token to the composables's xhibit token
+    nft_no_auth.safeTransferFrom(alice, xhibit, 0, 1, {"from": alice})
+
+    # when we call rootOwnerOfChild looking for who owns the child token
+    # (nft_no_auth, 0) it should see that the parent token is owned by a
+    # contract, and then call that contract's rootOwnerOfChild fn
+    # with (xhibit, parent_token_id)
+    # the result is the parent given by the composables rootOwnerOfChild fn
+    expected_root_owner = ERC998_MAGIC_VALUE[:-40] + ETH_ADDR[2:]
+    root_owner = xhibit.rootOwnerOfChild(nft_no_auth, 0)
+
+    assert root_owner == expected_root_owner

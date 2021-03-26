2ac2c33a3ac8e8e08473b8dd1d5083dd4c90957c
Edward Amor <edward.amor3@gmail.com>
2021-03-26 05:14:15 -0400

    test: add erc223_transfer_last fixture

--- tests/ERC/998/20/TopDown/conftest.py
+++ tests/ERC/998/20/TopDown/conftest.py
@@ -11,8 +11,14 @@ def token_223(alice, ERC223):
     return alice.deploy(ERC223, "Test Token 223", "TST223", 18)
 
 
+@pytest.fixture(scope="module")
+def token_223_transfer_last(alice, ERC223TransferLast):
+    return alice.deploy(ERC223TransferLast, "Test Token TL 223", "TSTTL223", 18)
+
+
 @pytest.fixture(scope="module", autouse=True)
-def setup(alice, token, token_223, xhibit):
+def setup(alice, token, token_223, token_223_transfer_last, xhibit):
     token._mint_for_testing(alice, 100 * 10 ** 18, {"from": alice})
     token_223._mint_for_testing(alice, 100 * 10 ** 18, {"from": alice})
+    token_223_transfer_last._mint_for_testing(alice, 100 * 10 ** 18, {"from": alice})
     xhibit.mint(alice, {"from": alice})
